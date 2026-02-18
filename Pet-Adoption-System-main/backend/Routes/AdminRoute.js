d1a2d275ff46cbf5a34129a5e3a940fb824fc801
ashish joshi <ashish63joshi@gmail.com>
2026-02-18 12:55:24 +0530

    Delete Pet-Adoption-System-main directory

--- Pet-Adoption-System-main/backend/Routes/AdminRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getCredentials } = require('../Controllers/AdminController');
-
-router.get('/credentials', getCredentials);
-
-module.exports = router;
--- Pet-Adoption-System-main/frontend/src/Components/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const ProtectedRoute = ({ children }) => {
-  const user = localStorage.getItem("token"); 
-
-  return user ? children : <Navigate to="/auth" replace />;
-};
-
-export default ProtectedRoute;
